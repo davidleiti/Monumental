a2971c17b9005384d61e19426ab11c61f454fd9c
Leiti Dávid <leiti26@gmail.com>
2019-06-17 00:05:08 +0300

    Implemented image use cases (supposedly)

--- Final/data/src/main/java/ubb/thesis/david/data/FirebaseImageStorage.kt
+++ Final/data/src/main/java/ubb/thesis/david/data/FirebaseImageStorage.kt
@@ -1,28 +1,88 @@
 package ubb.thesis.david.data
 
+import android.net.Uri
 import com.google.firebase.storage.FirebaseStorage
 import io.reactivex.Completable
 import io.reactivex.Single
+import io.reactivex.subjects.CompletableSubject
+import io.reactivex.subjects.SingleSubject
+import ubb.thesis.david.data.utils.debug
+import ubb.thesis.david.data.utils.info
 import ubb.thesis.david.domain.ImageStorage
+import java.io.File
 
 class FirebaseImageStorage : ImageStorage {
 
-    private val reference = FirebaseStorage.getInstance().getReference("images")
+    private val storage = FirebaseStorage.getInstance()
 
     override fun storeImage(userId: String, imageId: String, filePath: String): Completable {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val uploadTask = CompletableSubject.create()
+
+        val reference = storage.getReference("$userId/images/$imageId")
+
+        val fileUri = Uri.fromFile(File(filePath))
+        reference.putFile(fileUri)
+                .addOnSuccessListener {
+                    info(TAG_LOG, "Successfully uploaded image with id $imageId")
+                    uploadTask.onComplete()
+                }.addOnFailureListener { error ->
+                    debug(TAG_LOG, "Failed to upload image with id $imageId")
+                    uploadTask.onError(error)
+                }
+
+        return uploadTask
     }
 
-    override fun downloadImage(userId: String, imageUrl: String): Completable {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun downloadImage(userId: String, imageId: String, targetPath: String): Completable {
+        val downloadTask = CompletableSubject.create()
+        val targetFile = File(targetPath)
+
+        storage.getReference("$userId/images/$imageId").getFile(targetFile)
+                .addOnSuccessListener {
+                    logEvent("Saved image $imageId successfully to $targetPath")
+                    downloadTask.onComplete()
+                }.addOnFailureListener { error ->
+                    logEvent("Failed to save image $imageId with error ${error.message}")
+                    downloadTask.onError(error)
+                }
+
+        return downloadTask
     }
 
     override fun deleteImage(userId: String, imageId: String): Completable {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val deleteImageTask = CompletableSubject.create()
+
+        storage.getReference("$userId/images/$imageId").delete()
+                .addOnSuccessListener {
+                    logEvent("Deleted image $imageId successfully.")
+                    deleteImageTask.onComplete()
+                }.addOnFailureListener { error ->
+                    logEvent("Failed to delete image $imageId with error ${error.message}")
+                    deleteImageTask.onError(error)
+                }
+
+        return deleteImageTask
     }
 
     override fun getImageUrl(userId: String, imageId: String): Single<String> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val getUrlTask = SingleSubject.create<String>()
+
+        storage.getReference("$userId/images/$imageId").downloadUrl
+                .addOnSuccessListener { uri ->
+                    logEvent("Retrieved download url of image $imageId successfully.")
+                    getUrlTask.onSuccess(uri.toString())
+                }.addOnFailureListener { error ->
+                    logEvent("Failed to retrieve download url of image $imageId with error ${error.message}")
+                    getUrlTask.onError(error)
+                }
+
+        return getUrlTask
+    }
+
+    private fun logEvent(message: String) = debug(TAG_LOG, message)
+
+    companion object {
+        private const val TAG_LOG = "FirebaseImageStorageLogger"
     }
 
 }
--- Final/domain/src/main/java/ubb/thesis/david/domain/ImageStorage.kt
+++ Final/domain/src/main/java/ubb/thesis/david/domain/ImageStorage.kt
@@ -6,7 +6,7 @@ import io.reactivex.Single
 interface ImageStorage {
 
     fun storeImage(userId: String, imageId: String, filePath: String): Completable
-    fun downloadImage(userId: String, imageUrl: String): Completable
+    fun downloadImage(userId: String, imageId: String, targetPath: String): Completable
     fun deleteImage(userId: String, imageId: String): Completable
     fun getImageUrl(userId: String, imageId: String): Single<String>
 
